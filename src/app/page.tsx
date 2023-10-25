4d4c92ef7961a4ed8360d5232299e27c0c33606e
Laysa <laysa.silva@ccc.ufcg.edu.br>
2023-10-24 21:47:20 -0300

    add subsection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import { SubSection } from "@/components/SubSection";
+
 export default async function HomePage() {
-  return <div>Home</div>;
+  return (
+    <div>
+      {" "}
+      <SubSection
+        title="NOSSO CORE"
+        subtitle="Esse é o nosso core, a equipe responsável por organizar desde o início os eventos, 
+  manter os projetos da OpenDev e garantir que a comunidade permanceça seguindo em um caminho coerente."
+      />
+    </div>
+  );
 }
